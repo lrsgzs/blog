78138aadd5145afbc4555450ff5e0b2afb8bc3f6
刘镕硕 <99574908+lrsgzs@users.noreply.github.com>
2022-08-27 11:57:30 +0800

    add info.js

---
+++ info.js
@@ -0,0 +1,12 @@
+// 这是这个博客的配置信息
+
+
+// 博客名字
+var blog_title = "刘镕硕的博客";
+
+// 404页面提示文本
+var page_note_found_text = "页面不见了";
+
+// 文章GitHub仓库api地址
+// 如：https://api.github.com/repos/lrsgzs/blog_topics/contents/topics
+var gitckurl = "https://api.github.com/repos/lrsgzs/blog_topics/contents/topics";
